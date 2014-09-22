065a81967baf6b2134e5e064dbbac73e6a9f926d
Justine Jones <justinejones91@gmail.com>
2014-09-22 17:09:32 -0400

    added the method to retrieve follower data from client

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,2 +1,20 @@
 class Follower < ActiveRecord::Base
+	belongs_to :user
+
+	def self.follower_data(location_value, user_id, follower,)
+		self.create_with(
+			name: follower.name,
+			avatar: follower.profile_image_url,
+			location: follower.location,
+			latitude: location_value.first
+			longitude: location_value.second
+			user_id: user_id
+			).find_or_create_by(user_name: follower.user_name, user_id: user_id)
+		
+	end
+
+	def self.marker_data(name, location, user_name)
+		"<span class='friend-info'><strong>@#{screen_name}</strong><br>#{name}<br>#{location}<span>"
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
    match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   # get 'auth/failure' => redirect('/')
-  get 'welcome/followers', to: 'welcome#fetch_follower_data', as: 'fetch_followers'
+  get 'welcome/followers', to: 'welcome#follower_data', as: 'follower_data'
   match "log_out" => "sessions#destroy", :as => "log_out", via: [:get, :post]
   root to: "welcome#index"
 
