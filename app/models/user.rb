9d8b3e4e2936767b0bacff0220fc3e9f16c473a3
Justine Jones <justinejones91@gmail.com>
2014-09-22 17:29:43 -0400

    changed image to avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 			user.uid = auth['uid'] 
 			user.name = auth['info']['name'] || ''
 			user.address = auth['info']['location'] || ''
-			user.image = auth["info"]["image"] || ''
+			user.avatar = auth["info"]["image"] || ''
 			user.oauth_token = auth["credentials"]["token"] || ''
 			user.oauth_secret = auth["credentials"]["secret"] || ''
 			user.save!
