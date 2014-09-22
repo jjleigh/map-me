edea842b26849c62a2637e1a70ad44238d1998a0
Justine Jones <justinejones91@gmail.com>
2014-09-22 17:13:51 -0400

    fixed syntax error by adding missing commas

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -6,8 +6,8 @@ class Follower < ActiveRecord::Base
 			name: follower.name,
 			avatar: follower.profile_image_url,
 			location: follower.location,
-			latitude: location_value.first
-			longitude: location_value.second
+			latitude: location_value.first,
+			longitude: location_value.second,
 			user_id: user_id
 			).find_or_create_by(user_name: follower.user_name, user_id: user_id)
 		
