09ada515d81db89fb2185a9d0bcf94648ba58a54
Justine Jones <justinejones91@gmail.com>
2014-09-22 10:58:32 -0400

    added the method that creates a user with omni auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ActiveRecord::Base
+	 has_many :followers
+	# def self.from_omniauth(auth)
+ #    find_by_provider_and_uid(auth["provider"], auth["uid"]) || create_with_omniauth(auth)
+ #  end
+	def self.create_with_omniauth(auth)
+		create! do |user|
+			user.provider = auth['provider']
+			user.uid = auth['uid'] 
+			user.name = auth['info']['name'] || ''
+			user.address = auth['info']['location'] || ''
+			user.image = auth["info"]["image"] || ''
+			user.oauth_token = auth["credentials"]["token"] || ''
+			user.oauth_secret = auth["credentials"]["secret"] || ''
+			user.save!
+		end
+		
+	end
 end
