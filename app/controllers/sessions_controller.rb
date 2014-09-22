3be5b2ab5dc8ee80222d431801e66e537760e8c5
Justine Jones <justinejones91@gmail.com>
2014-09-22 00:35:46 -0400

    added the code for working with oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,21 @@
 class SessionsController < ApplicationController
   def create
+  	auth = request.env["omniauth.auth"]
+  user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+  session[:user_id] = user.id
+  redirect_to root_url, :notice => "Signed in!"
+
+		# auth = request.env['omniauth.auth']
+		# user = User.find_by_email(params[:email])
+		# if user && user.authenticate(params[:password])
+		# else
+		# 	flash.now[:alert] = "Invalid email or password"
+		# 	render :new
+		# end
   end
 
   def destroy
+  	session[:user_id] = nil
+		redirect_to root_url
   end
 end
