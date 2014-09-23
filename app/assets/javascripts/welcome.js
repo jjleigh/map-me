e914cb029505cba6df9a455537fa85e97e412126
Justine Jones <justinejones91@gmail.com>
2014-09-22 23:26:11 -0400

    removed map code

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,43 +1,43 @@
-var Map = {};
-Map.nearby_marker_img = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
+// var Map = {};
+// Map.nearby_marker_img = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
 
-Map.latitude = 43.6425662;
-Map.longitude = -79.3870568;
-function intializeMap() {
-	Map.options = {
-		zoom: 14,
-		center: new google.maps.LatLng(Map.longitude, Map.latitude),
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
+// Map.latitude = 43.6425662;
+// Map.longitude = -79.3870568;
+// function intializeMap() {
+// 	Map.options = {
+// 		zoom: 14,
+// 		center: new google.maps.LatLng(Map.longitude, Map.latitude),
+// 		mapTypeId: google.maps.MapTypeId.ROADMAP
+// 	};
 
-	Map.canvas = new google.maps.Map($('#map-canvas')[0], Map.options);
-	if (Map.showMarkers) {
-		var myMarker = new google.maps.Marker ({
-			position: new google.maps.LatLng(Map.longitude, Map.latitude),
-			map: Map.canvas
-		});
-	}
-}
+// 	Map.canvas = new google.maps.Map($('#map-canvas')[0], Map.options);
+// 	if (Map.showMarkers) {
+// 		var myMarker = new google.maps.Marker ({
+// 			position: new google.maps.LatLng(Map.longitude, Map.latitude),
+// 			map: Map.canvas
+// 		});
+// 	}
+// }
 
-function addMarkers(coords) {
-	coords.forEach(function (coord) {
-		var myMarker = new google.maps.Marker ({
-			position: new google.maps.LatLng(coord.longitude, coord.latitude),
-			map: Map.canvas,
-			icon: Map.nearby_marker_img
-		});
-	});
+// function addMarkers(coords) {
+// 	coords.forEach(function (coord) {
+// 		var myMarker = new google.maps.Marker ({
+// 			position: new google.maps.LatLng(coord.longitude, coord.latitude),
+// 			map: Map.canvas,
+// 			icon: Map.nearby_marker_img
+// 		});
+// 	});
 
-}
+// }
 
-$(document).on('ready page:load', function() {
+// $(document).on('ready page:load', function() {
 
-	if ($("#map-canvas").length) {
-		intializeMap();
-		if (Map.coords.length > 0) addMarkers(Map.coords);
-	}
+// 	if ($("#map-canvas").length) {
+// 		intializeMap();
+// 		if (Map.coords.length > 0) addMarkers(Map.coords);
+// 	}
 
-});
+// });
 
 
 
