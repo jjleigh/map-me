8972f2e927f25671d113b791588889ce3ca52354
Justine Jones <justinejones91@gmail.com>
2014-09-23 19:46:43 -0400

    fixed the syntax error

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -50,7 +50,7 @@
 function initialize() {
   var mapOptions = {
     zoom: 8,
-    center: new google.maps.LatLng(-79.3870568, 43.6425662;)
+    center: new google.maps.LatLng(-79.3870568, 43.6425662)
   };
 
   var map = new google.maps.Map(document.getElementById('map-canvas'),
