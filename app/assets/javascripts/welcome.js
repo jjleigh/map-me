d2b92471a8004cdb1e9c3528975d17ce4ac973b2
Justine Jones <justinejones91@gmail.com>
2014-09-24 13:39:08 -0400

    trying to fix syntax error

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,4 +1,14 @@
+// var Map = {};
+// Map.nearby_marker_img = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
 
+// Map.latitude = 43.6425662;
+// Map.longitude = -79.3870568;
+// function intializeMap() {
+	// Map.options = {
+// 		zoom: 14,
+// 		center: new google.maps.LatLng(Map.longitude, Map.latitude),
+// 		mapTypeId: google.maps.MapTypeId.ROADMAP
+// 	};
 
 // 	Map.canvas = new google.maps.Map($('#map-canvas')[0], Map.options);
 // 	if (Map.showMarkers) {
@@ -9,7 +19,25 @@
 // 	}
 // }
 
+// function addMarkers(coords) {
+// 	coords.forEach(function (coord) {
+// 		var myMarker = new google.maps.Marker ({
+// 			position: new google.maps.LatLng(coord.longitude, coord.latitude),
+// 			map: Map.canvas,
+// 			icon: Map.nearby_marker_img
+// 		});
+// 	});
 
+// }
+
+// $(document).on('ready page:load', function() {
+
+// 	if ($("#map-canvas").length) {
+// 		intializeMap();
+// 		if (Map.coords.length > 0) addMarkers(Map.coords);
+// 	}
+
+// });
 
 // // function geolocationSuccess(position){
 // // 	var latitude = position.coords.latitude;
@@ -21,7 +49,7 @@
 
 var Map = {};
 Map.showMarkers = true;
-Map.nearby_marker_img = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png';
+Map.nearby_marker_img = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
 
 
 function initialize() {
@@ -36,30 +64,31 @@ function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
-    // if (Map.showMarkers ){
-    // 	var MyMarkers = new google.maps.Maker ({
-    // 		position: new google.maps.LatLng(Map.latitude, Map.longitude),
-    // 		map: Map.canvas
-    // 	});
-    // }
+    if (Map.showMarkers ){
+    	var MyMarkers = new google.maps.Maker ({
+    		// position: new google.maps.LatLng(Map.latitude, Map.longitude)
+
+    	});
+    }
 }
 
-// function addMarkers(coords) {
-// 	coords.forEach(function(coord){
-// 		var myMarker = new google .maps.Marker ({
-// 			position: new google.maps.LatLng(coord.latitude, coord.longitude),
-// 			map: Map.canvas,
-// 			icon: Map.nearby_marker_img
-// 		})
-// 	})
-// }
+function addMarkers(coords) {
+	coords.forEach(function(coord){
+		var myMarker = new google .maps.Marker ({
+			position: new google.maps.LatLng(Map.latitude, Map.longitude),
+			map: Map.canvas
+			icon: Map.nearby_marker_img
+		})
+	})
+}
 
-// $(document).on('ready page:load', function() {
-// 	if ($('#map-canvas').length) {
-// 		initializeMap();
-// 		if (Map.coords.length > 0) addMarkers(Map.coords);
-// 	}
-// });
+
+    // handler = Gmaps.build('Google');
+    // handler.buildMap({internal: {id: 'map'}, provider: {styles: mapStyle}}, function(){
+    //   followerMarkers = handler.addMarkers(markers);
+    //   handler.bounds.extendWith(friendMarkers);
+    //   handler.fitMaptoBounds();
+    // })
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
