7dd68b007879c9290371f9d16a8e83ed313c15f6
Justine Jones <justinejones91@gmail.com>
2014-09-24 00:39:43 -0400

    added some zoom control options

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -54,6 +54,9 @@ function initialize() {
     center: new google.maps.LatLng(43.642, -79.387),
     panControl: false,
     zoomControl: true,
+    zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.LARGE,
+        position: google.maps.ControlPosition.LEFT_CENTER
     scaleControl: true
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
@@ -64,9 +67,9 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
-$('.map_canvas').on('shown',function(){      
-   google.maps.event.trigger(map, 'resize'); 
-});
+// $('.map_canvas').on('shown',function(){      
+//    google.maps.event.trigger(map, 'resize'); 
+// });
 
 
 
