006ab4d06714631bd23ad889c18d02312d5eb4dd
Justine Jones <justinejones91@gmail.com>
2014-09-23 20:20:31 -0400

    changed the coordinates

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -50,9 +50,9 @@
 var map;
 function initialize() {
   var mapOptions = {
-    zoom: 4,
-    // center: new google.maps.LatLng(-34.397, 150.644)
-    center: new google.maps.LatLng(41.850033, -87.6500523)
+    zoom: ,
+    center: new google.maps.LatLng(-34.397, 150.644)
+    // center: new google.maps.LatLng(41.850033, -87.6500523)
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
