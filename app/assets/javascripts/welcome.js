0fe6d5139a047362ea0af1f4a594b9be0f033661
Justine Jones <justinejones91@gmail.com>
2014-09-23 20:08:14 -0400

    added a new script

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -47,23 +47,14 @@
 // // 	console.log("there was an error");
 // // }
 
+var map;
 function initialize() {
   var mapOptions = {
-    zoom: 14,
-    center: new google.maps.LatLng(79.387, 43.642)
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644)
   };
-
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
+  map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 }
 
-function loadScript() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
-      'callback=initialize';
-  document.body.appendChild(script);
-}
-
-window.onload = loadScript;
-
+google.maps.event.addDomListener(window, 'load', initialize);
