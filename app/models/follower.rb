f285967259b2a938f4df3df0552ade6c426571b0
Justine Jones <justinejones91@gmail.com>
2015-03-02 15:50:46 -0500

    fixed follower data method

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,15 +1,14 @@
 class Follower < ActiveRecord::Base
-	belongs_to :user
+	belongs_to :user, dependent: :destroy
 
-	def self.follower_data(location_value, user_id, follower)
+	def self.store_follower_data(location_value, user_id, follower)
 		self.create_with(
 			name: follower.name,
-			avatar: follower.profile_image_url,
+			avatar: follower.profile_image_url.to_s,
 			location: follower.location,
 			latitude: location_value.first,
 			longitude: location_value.second,
 			user_id: user_id
-			).find_or_create_by(user_name: follower.user_name, user_id: user_id)
-		
+		).find_or_create_by(user_name: follower.user_name, user_id: user_id)
 	end
 end
