4ece894272b1035d36633a51bbc7bd866a7d26ee
Justine Jones <justinejones91@gmail.com>
2015-03-01 20:34:52 -0500

    changed env variable names

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
+	provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 	provider :instagram, ENV['INSTAGRAM_KEY'], ENV['INSTAGRAM_SECRET']
-	provider :google_oauth2, ENV['GOOGLE_PLUS_KEY'], ENV['GOOGLE_PLUS_SECRET']
+	provider :google_oauth2, ENV['GOOGLE_OAUTH2_KEY'], ENV['GOOGLE_OAUTH2_SECRET']
 end
