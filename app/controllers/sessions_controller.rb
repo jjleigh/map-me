e02e903323611fec2284e759aac8b8f8636c7563
Justine Jones <justinejones91@gmail.com>
2015-03-02 14:21:00 -0500

    added get followers call

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,9 @@ class SessionsController < ApplicationController
   def create
   	auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    user.get_followers
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "Signed in!"
-    
-    user.get_twitter_followers
+    redirect_to root_url, :notice => "Signed in!"   
   end
 
   def destroy
