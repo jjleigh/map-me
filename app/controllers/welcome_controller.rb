1a1bd45be14b4aa34e4d61fe97c96e6276c713de
Justine Jones <justinejones91@gmail.com>
2015-03-02 15:51:59 -0500

    added gon

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   def index
     if current_user && current_user.followers.present?
       gon.markers = []
-      gon.followers = current_user.followers.limit(70)
+      gon.followers = current_user.followers.limit(200)
   		gon.followers.each do |f|
   			gon.markers << [f.user_name, f.location, f.name, f.latitude, f.longitude, f.avatar]
   		end 
