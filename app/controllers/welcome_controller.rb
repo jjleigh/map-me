7856634e6c31da2167c52dfb8d51f7d445b7d859
Justine Jones <justinejones91@gmail.com>
2015-03-02 14:18:08 -0500

    using gon

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,13 +2,11 @@ class WelcomeController < ApplicationController
   
   def index
     if current_user && current_user.followers.present?
-  		@markers = []
-  		@followers = current_user.followers
-  		# @followers.each do |f|
-  		# 	marker_data = Follower.marker_data(f.user_name, f.location, f.name)
-  		# 	@markers << [marker_data, f.latitude, f.longitude, f.avatar]
-  		# end 
+      gon.markers = []
+      gon.followers = current_user.followers.limit(70)
+  		gon.followers.each do |f|
+  			gon.markers << [f.user_name, f.location, f.name, f.latitude, f.longitude, f.avatar]
+  		end 
   	end 
   end
-
 end
